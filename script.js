c79134ccc2a11915075f833774ed2112eaf14b9e
juancrfig <juancrfig@gmail.com>
2025-01-24 14:55:06 -0500

    Add book constructor

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(
+    name, 
+    description, 
+    author, 
+    year, 
+    numberOfPages,
+    recommendedBy
+) {
+    this.name = name;
+    this.description = description;
+    this.author = author;
+    this.year = year;
+    this.numberOfPages = numberOfPages;
+    this.recommendedBy = recommendedBy;
+}
+
+function addBookToLibrary() {
+    
+    myLibrary.push()
+}
